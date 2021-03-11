f273d9d093009fee9cea61316f9d14d5baa2d8b4
Robin Rawat <robinrawat7017@gmail.com>
2021-03-11 14:25:34 +0530

    fix @see https://github.com/realsirjoe/instagram-scraper/issues/166#issue-777655619

--- examples/get_account_following.py
+++ examples/get_account_following.py
@@ -2,7 +2,7 @@ from context import Instagram # pylint: disable=no-name-in-module
 from time import sleep
 
 instagram = Instagram()
-instagram.with_credentials('username', 'password', 'pathtocache')
+instagram.with_credentials('james_kumar_129', '4[PQ)yRE7.M;cq3', '/home/robin/projects/instagram-scraper/cache')
 instagram.login()
 
 sleep(2) # Delay to mimic user
